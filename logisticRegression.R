b1bf36e6756ede5d543d87a9a4f3d8a5cecc1d33
diogro <diogro@gmail.com>
2015-08-11 18:45:38 -0300

    graphs

--- logisticRegression.R
+++ logisticRegression.R
@@ -1,7 +1,14 @@
 library(rms)
+library(ggplot2)
+library(magrittr)
 data = read.csv("./exp_escolha.csv")
 
 model = lrm(choice ~ treatment, data)
 contrast(model, list(treatment = "rotten"), list(treatment = "fresh"))
+contrast(model, list(treatment = "rotten"), list(treatment = "dog"))
+contrast(model, list(treatment = "fresh"), list(treatment = "dog"))
 
-ggplot(Predict(model, treatment  = levels(data$treatment)))
+x = Predict(model, treatment  = levels(data$treatment))
+ggplot(x) + theme_classic()
+ggplot(data.frame(x), aes(treatment, yhat)) + geom_pointrange(aes(ymin = lower, ymax = upper)) + theme_classic() + labs(y = "Log odds of choosing rotten")
+
