a58b1b250f7ef1bd52f8b4c288c042b232fc5a3a
diogro <diogro@gmail.com>
2015-09-22 17:32:57 -0300

    consistent plts

--- logisticRegression.R
+++ logisticRegression.R
@@ -15,16 +15,16 @@ ggplot(x) + theme_classic()
 pvalues = data.frame( x = c(2, 2, 3, 3),  y = c(2.8, 2.9, 2.9, 2.8))
 odds_ratio = ggplot(data.frame(x), aes(treatment, yhat)) +
     geom_pointrange(aes(ymin = lower, ymax = upper)) +
-    labs(x = "\nTreatment", y = "Log odds ratio of choosing rotten\n") +
+    labs(x = "Treatment", y = "Log odds ratio of choosing rotten") +
     scale_x_discrete(labels = c("Dog food", "Fresh cricket", "Rotten cricket"), expand = c(0.2, 0)) +
     scale_y_continuous(breaks = -2:2) +
     geom_hline(yintercept = 0, linetype =  "dotted") +
     geom_path(data = pvalues, aes(x, y)) +
     annotate("text", x = 2.5, y = 3.1, label = "p < 0.01") +
-    annotate("text", x = 0.5, y = 2, label = "chooses\nrotten") +
-    annotate("text", x = 0.5, y = 0, label = "no\npreference") +
-    annotate("text", x = 0.5, y = -2, label = "chooses\nfresh")
-save_plot("./log_odds_ratio_feed_experiment.png", odds_ratio, base_height = 7)
+    annotate("text", x = 0.5, y = 2, label = "Chooses\nrotten") +
+    annotate("text", x = 0.5, y = 0, label = "No\npreference") +
+    annotate("text", x = 0.5, y = -2, label = "Chooses\nfresh") + theme(legend.position = c(0.8, 0.9), text = element_text(size = 20))
+save_plot("./log_odds_ratio_feed_experiment.png", odds_ratio, base_height = 7, base_aspect_ratio = 1.1)
 
 
 survival <- data.frame(t(read.csv("./survival.csv")))
